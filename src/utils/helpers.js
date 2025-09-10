d175c0bb54a0c18a8cf359a60d167beca0cac5e5
Ash <105349273+Ashfaith@users.noreply.github.com>
2025-09-10 21:12:07 +1200

    running production build

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,22 +1,24 @@
 export function toUrlSlug(str) {
-    return str
-      .toLowerCase() 
-      .trim()
-      .replace(/[^a-z0-9\s-]/g, "")
-      .replace(/\s+/g, "-")
-      .replace(/-+/g, "-");
-  }
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+}
 
 export function capitaliseFirstLetter(str) {
   return String(str).charAt(0).toUpperCase() + String(str).slice(1);
 }
 
 export const limitTitleLength = (title, limit, length) => {
-    const originalStr = title;
-    const strToArr = originalStr.split(" ");
-    let shortenedStr = originalStr;
-    if (strToArr.length > limit) {
-      shortenedStr = strToArr.slice(0, length).join(" ") + "...";
-    }
-    return shortenedStr;
-  };
+  const originalStr = title;
+  const strToArr = originalStr.split(" ");
+  let shortenedStr = originalStr;
+  if (strToArr.length > limit) {
+    shortenedStr = strToArr.slice(0, length).join(" ") + "...";
+  }
+  return shortenedStr;
+};
+
+//##
